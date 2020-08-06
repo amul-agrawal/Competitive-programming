4b6b85ba33598d097a7b9521066a558305cb663f
amul-agrawal <amul.agrawal@students.iiit.ac.in>
2020-08-06 14:13:29 +0530

    sorting using lambdas

---
+++ lambdas/sorting/WithSet.cpp
@@ -0,0 +1,69 @@
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ll long long int
+#define ld long double
+#define ordered_set tree < ll ,  null_type ,  less<ll> ,  rb_tree_tag ,  tree_order_statistics_node_update >
+//added two more functions to set
+//(1)*(set.find_by_order(k))[kth element in the sorted set] 
+//(2)set.order_of_key(k)[count of elements strictly less than k]
+
+typedef vector< int > vi;
+typedef vector<long long> lvi;
+typedef vector< vector<int> > vvi;
+typedef vector< vector<long long> > lvvi;
+typedef pair< int,int > ii;
+typedef pair< long long,long long > lii;
+typedef vector<pair<int,int>> vii;
+typedef vector<pair<long long,long long>> lvii;
+typedef vector<vector<pair<int,int>>> vvii;
+typedef vector<vector<pair<long long,long long>>> lvvii;
+typedef vector<bool> vb;
+
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define tr(c,i) for(auto i = (c).begin(); i != (c).end(); i++)//remember i is an iterator
+#define tc(t) int (t); cin>>(t);while((t)--)
+#define ff first
+#define ss second
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cerr << *it << " is " << a << endl;
+	err(++it, args...);
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#define mod 1000000007
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+long long mos() { return 0LL; }
+template<typename T, typename... Args>
+T mos(T a, Args... args) { return ((a + mos(args...))%mod + mod)%mod; }
+
+long long mop() { return 1LL; }
+template<typename T, typename... Args>
+T mop(T a, Args... args) { return (a*mop(args...))%mod; }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);	
+	auto cmp = [](int a,int b) { return a>b; } ; 
+	set<int,decltype(cmp)> st(cmp);
+	for(int i=-15;i<10;i++)
+	{
+		st.insert(i);
+	}
+	for(auto curr:st)
+	{
+		cout<<curr<<endl;
+	}
+	return 0;
+}	
