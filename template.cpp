16012f3e49bcdc6cd21dc863dbe1bf060a59e572
amul-agrawal <amul.agrawal@students.iiit.ac.in>
2020-09-29 15:38:50 +0530

    update in template

--- template.cpp
+++ template.cpp
@@ -1,3 +1,4 @@
+
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -15,44 +16,66 @@ typedef vector<long long> lvi;
 typedef vector< vector<int> > vvi;
 typedef vector< vector<long long> > lvvi;
 typedef pair< int,int > ii;
-typedef pair< long,long > lii;
+typedef pair< long long,long long > lii;
 typedef vector<pair<int,int>> vii;
-typedef vector<pair<long,long>> lvii;
+typedef vector<pair<long long,long long>> lvii;
 typedef vector<vector<pair<int,int>>> vvii;
-typedef vector<vector<pair<long,long>>> lvvii;
+typedef vector<vector<pair<long long,long long>>> lvvii;
+typedef vector<bool> vb;
 
+// #ifdef ONLINE_JUDGE 
+// #define endl '\n'
+// #endif
 #define pb push_back
 #define all(c) (c).begin(),(c).end()
 #define tr(c,i) for(auto i = (c).begin(); i != (c).end(); i++)//remember i is an iterator
 #define tc(t) int (t); cin>>(t);while((t)--)
 #define ff first
 #define ss second
-#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#ifdef ONLINE_JUDGE
+	#define error(args...) 0
+#else
+	#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#endif
 void err(istream_iterator<string> it) {}
 template<typename T, typename... Args>
 void err(istream_iterator<string> it, T a, Args... args) {
-	cerr << *it << " is " << a << endl;
+	cerr <<"[ "<< *it << " = " << a << " ]"<< endl;
 	err(++it, args...);
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
-#define mod 1000000007
+const long long mod = 1e9 + 7;
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-long long mod_sum() { return 0LL; }
+long long mos() { return 0LL; }
 template<typename T, typename... Args>
-T mod_sum(T a, Args... args) { return ((a + mod_sum(args...))%mod + mod)%mod; }
+T mos(T a, Args... args) { return ((a + mos(args...))%mod + mod)%mod; }
 
-long long mod_product() { return 1LL; }
+long long mop() { return 1LL; }
 template<typename T, typename... Args>
-T mod_product(T a, Args... args) { return (a*mod_product(args...))%mod; }
+T mop(T a, Args... args) { return (a*mop(args...))%mod; }
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 
 signed main()
 {
 	ios_base::sync_with_stdio(false);
-    	cin.tie(NULL);	
-    
+    cin.tie(NULL);	
+	
 	return 0;
 }	
+
+
+// WA
+// 1. overflow
+// 2. re-initialize global variables for every test case.
+// 3. edge cases like n=1
+
+// Run time error
+// 1. division by zero.
+// 2. array bounds.
+
+// TLE
+// 1. uncomment that #define endl '\n' line
