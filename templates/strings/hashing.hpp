7fabaf0f0c2cf6a969994b0a6c2cc39fa4e003cc
amul-agrawal <amul.agrawal@students.iiit.ac.in>
2021-05-27 00:07:58 +0530

    added string hash

---
+++ templates/strings/hashing.hpp
@@ -0,0 +1,52 @@
+// https://github.com/kth-competitive-programming/kactl/blob/main/content/strings/Hashing-codeforces.h
+typedef uint64_t ull;
+static int C; // initialized below
+
+// Arithmetic mod two primes and 2^32 simultaneously.
+// "typedef uint64_t H;" instead if Thue-Morse does not apply.
+template<int M, class B>
+struct A {
+	int x; B b; A(int x=0) : x(x), b(x) {}
+	A(int x, B b) : x(x), b(b) {}
+	A operator+(A o){int y = x+o.x; return{y - (y>=M)*M, b+o.b};}
+	A operator-(A o){int y = x-o.x; return{y + (y< 0)*M, b-o.b};}
+	A operator*(A o) { return {(int)(1LL*x*o.x % M), b*o.b}; }
+	explicit operator ull() { return x ^ (ull) b << 21; }
+};
+typedef A<1000000007, A<1000000009, unsigned>> H;
+
+struct HashInterval {
+	vector<H> ha, pw;
+	HashInterval(string& str) : ha(str.length()+1), pw(ha) {
+		pw[0] = 1;
+		for(int i=0;i<str.length();i++)
+			ha[i+1] = ha[i] * C + str[i],
+			pw[i+1] = pw[i] * C;
+	}
+	H hashInterval(int a, int b) { // hash [a, b)
+		return ha[b] - ha[a] * pw[b - a];
+	}
+};
+
+vector<H> getHashes(string& str, int length) {
+	if (str.length() < length) return {};
+	H h = 0, pw = 1;
+	for(int i=0;i<length;i++)
+		h = h * C + str[i], pw = pw * C;
+	vector<H> ret = {h};
+	for(int i=length;i<str.length();i++) {
+		ret.push_back(h = h * C + str[i] - pw * str[i-length]);
+	}
+	return ret;
+}
+
+H hashString(string& s){H h{}; for(char c:s) h=h*C+c;return h;}
+
+#include <sys/time.h>
+int main() {
+	timeval tp;
+	gettimeofday(&tp, 0);
+	C = (int)tp.tv_usec; // (less than modulo)
+	assert((ull)(H(1)*2+1-3) == 0);
+	// ...
+}
