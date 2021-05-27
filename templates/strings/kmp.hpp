d129b078cadcf5eef615d30e3d5f470bfd9556ae
amul-agrawal <amul.agrawal@students.iiit.ac.in>
2021-05-27 23:08:48 +0530

    corrected kmp

--- templates/strings/kmp.hpp
+++ templates/strings/kmp.hpp
@@ -6,7 +6,7 @@ vector<int> prefix_function(string s)
 	vector<int> pi(n);
 	for(int i=1;i<n;i++)
 	{
-		int j = p[i-1];
+		int j = pi[i-1];
 		while(j > 0 and s[i] != s[j])
 			j = pi[j-1];
 		if(s[i] == s[j])
