388950f290e008ba83ad7803f7dcce85da4e760e
amul-agrawal <amul.agrawal@students.iiit.ac.in>
2021-06-13 12:22:58 +0530

    updated number theory

--- templates/number-theory/misc.hpp
+++ templates/number-theory/misc.hpp
@@ -11,6 +11,21 @@
 // - a ^ (phi(m)) = 1(mod m), where phi(m) is number of coprimes to m less than m.
 // phi(prime) = prime - 1.
 
+// stars and bars: (x_1 + x_2 .. x_n = r) s.t. x_i >= 0.
+// solution: (n+r-1)C(r-1).
+
+// Trick
+// Count number of arrays s.t. a[i+1] >= a[i] and 1 <= a[i] <= k.
+// difference array: 1 <= i < n, dif[i] > 0. dif[i] = a[i] - a[i-1].
+// Sum of diff array <= k - 1
+// x_1, x_2, .. x_n = dif_1, dif_2, .. dif_n.
+// create x_0 = which denotes the 1st character. 
+// create x_n+1 = which denotes the left sum to k - 1.
+// therefore, answer = (n+1+k-1)C(n)
+
+
+// Upper bound for number of divisors of x is x^1/3.
+
 
 // factorization
 factorization.push_back(1);
