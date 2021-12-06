b2d62b93e1d1c9075201afd3b90939e3b604a495
amul-agrawal <amul.agrawal@students.iiit.ac.in>
2021-12-06 23:18:03 +0530

    added algebra library and updated sparse table

--- templates/data-structures/sparse-table.hpp
+++ templates/data-structures/sparse-table.hpp
@@ -5,32 +5,25 @@ class SparseTable
 {
 	vector<vector<T>> st;
 	vector<int> log;
-	int n, logn;
+	int N, logN;
 public:
-	SparseTable(int N = 1e6 + 13, int logN = 20)
-	{
-		st.resize(N, vector<T>(logN));
-		log.resize(N + 1, 0);
-		for(int i=2;i<=N;i++) log[i] = log[i/2] + 1;
-		n = N;
-		logn = logN;
-	}
 	// define your function here, eg: max, min, sum
-	T f(T left,T right) 
-	{
+	T f(T left,T right) {
 		return max(left, right);
 	}
-	void build(vector<T> arr) 
-	{
-		for (int i = 0; i < n; ++i)
+	SparseTable(vector<T> arr, int n1 = 1e6 + 13) {
+		logN = log2(n1) + 1; N = n1;
+		st.resize(N + 1, vector<T>(logN + 1));
+		log.resize(N + 1, 0);
+		for(int i=2;i<=N;i++) 
+			log[i] = log[i/2] + 1;
+		for(int i = 1; i <= N; i++) 
 			st[i][0] = arr[i];
-
-		for(int j=1; j <= logn; j++)
-			for (int i = 0; i + (1 << j) <= n; i++)
+		for(int j=1; j <= logN; j++)
+			for (int i = 1; i + (1 << j) <= N + 1; i++)
 		        st[i][j] = f(st[i][j-1], st[i + (1 << (j - 1))][j - 1]);
 	}
-	T query(int L, int R)
-	{
+	T query(int L, int R) {
 		int j = log[R - L + 1];
 		return f(st[L][j], st[R - (1 << j) + 1][j]);
 	}
